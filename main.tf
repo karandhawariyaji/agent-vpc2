b3d157820cdbc0112adf6ce409a9285cd1cff41e
karandhawariyaji <karandhawariyaji@gmail.com>
2025-03-04 02:44:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_compute_network" "vpc" {
+resource "google_compute_network" "vpc" { 
   project                 = var.project_id
   name                    = var.vpc_name
   auto_create_subnetworks = true
